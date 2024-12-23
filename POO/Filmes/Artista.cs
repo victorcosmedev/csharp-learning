7284d2a5dce0d0f769b94b405a3a867dc59e6a47
victorcosmedev <victorcosme.profissional@gmail.com>
2024-12-22 21:53:38 -0300

    Item #8 - Exercício Filme Artista

---
+++ POO/Filmes/Artista.cs
@@ -0,0 +1,36 @@
+﻿namespace POO.Filmes;
+
+class Artista
+{
+    public string Nome { get; }
+    public int Idade { get; }
+    private List<Filme> filmesEmQueAtuou;
+
+    public Artista(string nome, int idade)
+    {
+        this.Nome = nome;
+        this.Idade = idade;
+        this.filmesEmQueAtuou = new();
+    }
+
+    public void AdicionarFilme(Filme filme)
+    {
+        this.filmesEmQueAtuou.Add(filme);
+    }
+
+    public void AdicionarFilme(List<Filme> filmes)
+    {
+        this.filmesEmQueAtuou.AddRange(filmes);
+    }
+
+    public void ExibirInformacoes()
+    {
+        Console.WriteLine($"{this.Nome} - {this.Idade} anos");
+        Console.WriteLine("Filmes em que atuou");
+        foreach(Filme f in filmesEmQueAtuou)
+        {
+            Console.WriteLine(f.DescricaoDetalhada);
+            Console.WriteLine("========================");
+        }
+    }
+}
